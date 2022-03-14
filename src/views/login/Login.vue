ab260f665ae9d23f22bb9ccef4eef1b8a00581ef
DunnLiu <15817294245@163.com>
2022-03-14 20:07:06 +0800

    'init'

---
+++ src/views/login/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <n-date-picker v-model:value="timestamp" type="datetime" clearable />
+    <pre>{{ JSON.stringify(timestamp) }}</pre>
+    <n-button type="success">
+        Success
+    </n-button>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue'
+    import {NDatePicker, NButton} from 'naive-ui'
+    const timestamp =  ref(1183135260000)
+</script>
